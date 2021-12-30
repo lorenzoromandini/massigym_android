d2b205de64974acdddc6bdec4ee8280d96daa5c7
Lorenzo Romandini <lorenzoromandini@live.it>
2021-12-30 22:48:00 +0100

    Login, Registration and BottomNavBar layouts

--- app/src/main/java/com/example/massigym_android/MainActivity.kt
+++ app/src/main/java/com/example/massigym_android/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.massigym_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.massigym_android.databinding.ActivityMainBinding
+import com.example.massigym_android.ui.HomeFragment
+import com.example.massigym_android.ui.auth.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var user : FirebaseUser? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        user = FirebaseAuth.getInstance().currentUser
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        if (user == null) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        else{
+            val intent = Intent(this, HomeFragment::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/massigym_android/SplashActivity.kt
+++ app/src/main/java/com/example/massigym_android/SplashActivity.kt
@@ -20,4 +20,6 @@ class SplashActivity : AppCompatActivity() {
           2000
       )
     }
+
+
 }
