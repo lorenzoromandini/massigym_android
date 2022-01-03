7bc32e7145decd28019452b00cab9f6ddb63a74c
Lorenzo Romandini <lorenzoromandini@live.it>
2022-01-03 22:16:29 +0100

    Home page (working)

--- app/src/main/java/com/example/massigym_android/HomeFragment.kt
+++ app/src/main/java/com/example/massigym_android/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.massigym_android.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,9 +21,33 @@ class HomeFragment : Fragment() {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        binding.categoryCardio.categoryTitle.setText("Porcoooo")
+        binding.categoryCardio.categoryTitle.setText("Cardio")
+        setCategoryImage(binding.categoryCardio.categoryImage, getString(R.string.cardioImageUrl))
+        binding.categoryCardio.categoryOverview.setText(getString(R.string.cardioTopDescription))
+        binding.categoryCardio.categoryDescription.setText(getString(R.string.cardioTopDescription))
+
+        binding.categoryLegs.categoryTitle.setText("Legs")
+        setCategoryImage(binding.categoryLegs.categoryImage, getString(R.string.legsImageUrl))
+        binding.categoryLegs.categoryOverview.setText(getString(R.string.legsTopDescription))
+        binding.categoryLegs.categoryDescription.setText(getString(R.string.legsBottomDescription))
+
+        binding.categoryArms.categoryTitle.setText("Arms")
+        setCategoryImage(binding.categoryArms.categoryImage, getString(R.string.armsImageUrl))
+        binding.categoryArms.categoryOverview.setText(getString(R.string.armsTopDescription))
+        binding.categoryArms.categoryDescription.setText(getString(R.string.armsBottomDescription))
+
 
         return binding.root
+    }
+
+    private fun setCategoryImage(destination: ImageView, url: String) {
+        Glide.with(this)
+            .load(url)
+            .centerCrop()
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .skipMemoryCache(true)
+            .dontAnimate()
+            .into(destination);
     }
 
 }
